6e20dc427380c69dd2261e0eb84edf3d4bdc2b50
CliveSlatter <cslatter@farnborough.ac.uk>
2021-01-12 16:49:17 +0000

    The majority of the HTML elements were still inside the <head> </head> tag rather than in the <body> </body> sections and were moved. The text fields for username and password were not named which means the formData property has nothing to send (fixed). The login form now works.

--- resources/client/js/Login.js
+++ resources/client/js/Login.js
@@ -1,21 +1,21 @@
 function postUserLogin() {
     console.log("Invoked postUserLogin() ");
 
-    let url = "/user/login";
+    let url = "/users/login";
     let formData = new FormData(document.getElementById('loginForm'));
 
     fetch(url, {
-        method: "POST",
+        method: "post",
         body: formData,
     }).then(response => {
         return response.json();                 //now return that promise to JSON
-}).then(response => {
+    }).then(response => {
         if (response.hasOwnProperty("Error")) {
-        alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
-    } else {
-        Cookies.set("token", response.token);
-        Cookies.set("username", response.username);
-        window.open("home.html", "_self");       //open index.html in same tab
-    }
-});
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.set("token", response.token);
+            Cookies.set("username", response.username);
+            window.open("home.html", "_self");       //open index.html in same tab
+        }
+    });
 }
